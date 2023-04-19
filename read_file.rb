a8ed62b8ccae70f8be4028c99740a5b5e9c8ccbb
Boichuk Bozhena <vogelweide00@gmail.com>
2023-04-19 20:26:35 +0300

    Lab 6

--- read_file.rb
+++ read_file.rb
@@ -1,99 +1,39 @@
-class HashTable
-  def initialize
-    @size = 200
-    @table = Array.new(@size)
-  end
+source_code = File.read(__FILE__)
 
-  def hash(key, i)
-    (key.to_s.bytes.sum + i) % @size
-  end
-
-  def insert(key, value)
-    i = 0
-    index = hash(key, i)
-
-    while @table[index]
-      i += 1
-      index = hash(key, i)
-    end
-
-    @table[index] = { key => value }
-  end
-
-  def search(key)
-    i = 0
-    index = hash(key, i)
-
-    while @table[index] && @table[index].keys.first != key
-      i += 1
-      index = hash(key, i)
-    end
-    @table[index][key]
-  end
-
-  def compare_simple_vs_sorted(file)
-    simple_table = HashTable.new
-    sorted_table = SortedList.new
-
-    simple_ops = 0
-    sorted_ops = 0
-
-    File.foreach(file) do |line|
-      key, value = line.chomp.split(' ')
-      simple_start_time = Time.now
-      simple_table.insert(key, value)
-      simple_end_time = Time.now
-      simple_ops += (simple_end_time - simple_start_time) * 1000000
-
-      sorted_start_time = Time.now
-      sorted_table.insert(key, value)
-      sorted_end_time = Time.now
-      sorted_ops += (sorted_end_time - sorted_start_time) * 1000000
-    end
-
-    s = sorted_table.search('apple')
-    h = hash_table.search('apple')
-    puts "Hash search result: #{h}"
-    puts "Sorted search result: #{s}"
-
-    simple_average_ops = simple_ops / File.foreach(file).count
-    sorted_average_ops = sorted_ops / File.foreach(file).count
-
-    puts "Simple HashTable average: #{simple_average_ops}"
-    puts "Sorted HashTable average: #{sorted_average_ops}"
+def condition
+  if x > 5
+    puts "x is greater than 5"
+  else
+    puts "x is less than or equal to 5"
   end
 end
 
-class SortedList
-  def initialize
-    @list = []
-  end
-
-  def insert(key, value)
-    i = 0
-    while i < @list.size && @list[i][0] < key
-      i += 1
-    end
-    @list.insert(i, [key, value])
-  end
-
-  def search(key)
-    i = 0
-    while i < @list.size && @list[i][0] < key
-      i += 1
-    end
-    i < @list.size && @list[i][0] == key ? @list[i][1] : nil
+def condition_2
+  if x < 5
+    puts "x is less than or equal to 5"
+  else
+    puts "x is greater than 5"
   end
 end
 
-hash_table = HashTable.new
-sorted_table = SortedList.new
+modified_code = source_code.gsub(/if\s+(.+?)\s+[\n\s]+(.+?)\s+[\n\s]+else\s+[\n\s]+(.+?)\s+[\n\s]+end/m) do |match|
+  condition = Regexp.last_match(1)
+  if_branch = Regexp.last_match(2)
+  else_branch = Regexp.last_match(3)
+  "unless #{condition}\n    #{else_branch}\n  else\n    #{if_branch}\n  end"
+end
 
-hash_table.insert("key2", "value2")
-sorted_table.insert("key1", "value1")
 
-hash_result = hash_table.search("key2")
-sorted_result = sorted_table.search("key1")
+File.open('modified_code.txt', 'w') { |file| file.write(modified_code) }
 
-puts "HashTable search result: #{hash_result}"
-puts "SortedList search result: #{sorted_result}"
+File.open('modified_code.txt', 'r+') do |f|
+  lines = f.readlines
+
+  lines.slice!(0, 2)
+  lines.slice!(-22, 22)
+
+  f.rewind
+  f.truncate(0)
+
+  f.write(lines.join)
+end
